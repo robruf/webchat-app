994b4f99a17d8269865d9753b9f58ce3d2104797
robruf <robruf93@yahoo.com>
2018-07-04 12:33:45 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,32 @@
+//Requirements
+var express = require("express");
+var socket = require("socket.io");
+
+// App setup
+
+var app = express();
+
+var server = app.listen(3000, function(){
+    console.log("Server is now running");
+});
+
+// Looks for static pages in "public" folder
+app.use(express.static("public"));
+
+//Initialize socket on server
+var io = socket(server);
+
+//Listens for connections and creates a socket for each one
+io.on("connection", function(socket) {
+    console.log("New connection made from " + socket.id);
+
+    //Emits message to all sockets when received
+    socket.on("chatMessage", function(data) {
+        io.sockets.emit("chatMessage", data);
+    })
+
+    socket.on("typing", function(data) {
+        socket.broadcast.emit("typing", data);
+    });
+});
+
---
+++ public/script.js
@@ -0,0 +1,39 @@
+var username = prompt("Enter your username");
+
+var socket = io.connect("http://localhost:3000");
+
+var messages = document.getElementById("messages");
+var msg = document.getElementById("msg");
+var btn = document.getElementById("send");
+var broadcast = document.getElementById("broadcast");
+
+function sendMessage() {
+    
+    socket.emit("chatMessage", {
+        name: username,
+        message: msg.value
+    });
+};
+
+btn.addEventListener("click", function() {
+    sendMessage();
+    msg.value = "";
+});
+
+msg.addEventListener("keypress", function(event){
+    socket.emit("typing", username);
+
+    if(event.which==13) {
+        sendMessage();
+        msg.value = "";
+    };
+});
+
+socket.on("chatMessage", function(data) {
+    broadcast.innerHTML = "";
+    messages.innerHTML += "<p><strong>" + data.name + "</strong>   " + data.message + "</p>";
+});
+
+socket.on("typing", function(data) {
+    broadcast.innerHTML = "<span><em>" + data + " is typing a message...</em></span>";
+});
